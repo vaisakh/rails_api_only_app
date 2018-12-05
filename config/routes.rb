7fe8ef8eb629cd9f56e6035641d594ab77b40472
vaisakh <mail.vaisakhvm@gmail.com>
2018-12-05 16:46:40 +0530

    Initial commit to something awesome

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  api_version(:module => "V1", :path => {:value => "v1"}) do
+    constraints subdomain: 'api' do
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
