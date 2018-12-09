17ddaf6f4953c47ebfeb21f5d7be0c04a3bcda7c
vaisakh <mail.vaisakhvm@gmail.com>
2018-12-09 19:48:34 +0530

    apartment configuration

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -48,7 +48,7 @@ Apartment.configure do |config|
   #   end
   # end
   #
-  config.tenant_names = lambda { ToDo_Tenant_Or_User_Model.pluck :database }
+  config.tenant_names = lambda { Account.pluck :subdomain }
 
   # PostgreSQL:
   #   Specifies whether to use PostgreSQL schemas or create a new database per Tenant.
